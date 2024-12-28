7de11ecef57b2b18b778023ba8adac36c154842d
me_me <ichshlanger2@gmail.com>
2024-12-28 19:52:47 +0300

    NEW: conv3d implemented

--- conv.cpp
+++ conv.cpp
@@ -32,3 +32,27 @@ Matrix Conv2d(const Matrix& X, const Matrix& F) {
 
   return res;
 }
+
+Tensor Conv3d(const Tensor& X, const Tensor& F) {
+  Tensor res = Tensor(X.size() - F.size() + 1,
+                      Matrix(X[0].size() - F[0].size() + 1,
+                      std::vector<float>(X[0][0].size() - F[0][0].size() + 1)));
+  for (size_t i {0}; i < res.size(); ++i) {
+    for (size_t j {0}; j < res[0].size(); ++j) {
+      for (size_t k {0}; k < res[0][0].size(); ++k) {
+        float tmp {0.f};
+
+        for (size_t n {0}; n < F.size(); ++n) {
+          for (size_t m {0}; m < F[0].size(); ++m) {
+            for (size_t s {0}; s < F[0][0].size(); ++s) {
+              tmp += X[i+n][j+m][k+s] * F[n][m][s];
+            }
+          }
+        }
+        res[i][j][k] = tmp;
+      }
+    }
+  }
+
+  return res;
+}
--- conv.hpp
+++ conv.hpp
@@ -6,10 +6,10 @@
 #include <memory>
 
 using Matrix = std::vector<std::vector<float>>;
-using Tensor = std::vector<std::vector<std::vector<float>>>;
+using Tensor = std::vector<Matrix>;
 
 Matrix Conv1d(const Matrix& X, const Matrix& F);
 Matrix Conv2d(const Matrix& X, const Matrix& F);
-//Matrix& Conv3d(const Matrix& X, const Matrix& F);
+Tensor Conv3d(const Tensor& X, const Tensor& F);
 
 #endif // CONV_HPP
